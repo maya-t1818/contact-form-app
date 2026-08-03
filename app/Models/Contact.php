120dbf2196ac5eb2671e2136ead313bbab4f2c25
Takeuchi maya <take18181225@yahoo.co.jp>
2026-08-04 02:52:24 +0900

    マイグレーション #1

---
+++ app/Models/Contact.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'first_name',
+        'last_name',
+        'gender',
+        'email',
+        'tell',
+        'address',
+        'building',
+        'detail',
+    ];
+
+    public function category()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Contact;
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $tagIds = Tag::pluck('id');
+
+        Contact::factory(20)->create()->each(function ($contact) use ($tagIds) {
+            $contact->tags()->attach($tagIds->random(rand(1, 3)));
+        });
+
+
+    }
+}
